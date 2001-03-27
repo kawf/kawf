4b256e803b29be4e25f8659d7d36a11d9fe4a225
jerdfelt <jerdfelt>
2001-03-27 23:48:35 +0000

    New API updates New table updates

--- user/flat.php
+++ user/flat.php
@@ -1,28 +1,25 @@
 <?php
 
-$fdb = "forum_" . $forum['shortname'];
-
 while (list($key) = each($indexes)) {
-
-  $sql = "select count(*) from messages" . $indexes[$key]['iid'];
-  $result = mysql_db_query($fdb, $sql) or sql_error($sql);
+  $sql = "select count(*) from f_messages" . $indexes[$key]['iid'];
+  $result = mysql_query($sql) or sql_error($sql);
 
   list($nummids) = mysql_fetch_row($result);
   $curmid = 0;
 
   $nummessages = 0;
   while ($curmid < $nummids) {
-    $sql = "select mid,state from messages" . $indexes[$key]['iid'] . " order by mid limit $nummessages,1000;";
-    $result = mysql_db_query($fdb, $sql) or sql_error($sql);
+    $sql = "select mid,state from f_messages" . $indexes[$key]['iid'] . " order by mid limit $nummessages,1000;";
+    $result = mysql_query($sql) or sql_error($sql);
 
     $curmid += mysql_num_rows($result);
 
     while (list($mid,$state) = mysql_fetch_row($result)) {
       $nummessages++;
-      if ($state == 'Deleted')
+      if ($state == 'Deleted' || $state == 'Moderated')
         continue;
 ?>
-<a href="<?php echo $urlroot . "/" . $forum['shortname'] . "/msgs/" . $mid; ?>.phtml">&nbsp;</a><br>
+<a href="<?php echo "/" . $forum['shortname'] . "/msgs/" . $mid; ?>.phtml">&nbsp;</a><br>
 <?php
     }
 
