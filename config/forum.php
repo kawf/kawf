9a97f1d6c98c0acd775b81557d0b5e3e1c2dd0e2
jerdfelt <jerdfelt>
2001-04-12 01:50:47 +0000

    New example forums.php file

--- config/forum.php
+++
@@ -1,5 +0,0 @@
-<?php
-ini_set('include_path', '/web/php.new/kawf:/web/audiworld.com/forum');
-
-include('main.php');
-?>
---
+++ config/forums.php
@@ -0,0 +1,9 @@
+<?php
+$srcroot = "/home/jerdfelt/software";
+$template_dir = "/web/audiworld.com/forums/dev/templates/";
+
+# We prod and dev for the different version
+$config = "config";
+
+include("$srcroot/kawf/user/main.php");
+?>
