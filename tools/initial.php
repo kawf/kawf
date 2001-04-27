27bc39b54a123e00ea19cfd8f18525eb8eec0b21
jerdfelt <jerdfelt>
2001-04-27 02:51:52 +0000

    Setup sane include path Select database Add user database

--- tools/initial.php
+++ tools/initial.php
@@ -1,12 +1,20 @@
 #!/usr/bin/php -q
 <?php
 
+/* First setup the path */
+$include_path = "..:../include:../../php";
+$old_include_path = ini_get("include_path");
+if (!empty($old_include_path))
+  $include_path .= ":" . $old_include_path;
+ini_set("include_path", $include_path);
+
+include("config.inc");
 include("sql.inc");
 include("user/tables.inc");
 
 set_time_limit(0);
 
-sql_open();
+sql_open($database);
 
 sql_query($create_forums_table);
 sql_query($create_visits_table);
@@ -15,5 +23,6 @@ sql_query($create_dupposts_table);
 sql_query($create_unique_table);
 sql_query($create_tracking_table);
 sql_query($create_update_table);
+sql_query($create_users_table);
 
 ?>
