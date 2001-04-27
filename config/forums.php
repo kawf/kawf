7e49cd0603516e2dd221a77fd596b581d05cb2a6
jerdfelt <jerdfelt>
2001-04-27 02:45:35 +0000

    Update comments, add default style

--- config/forums.php
+++ config/forums.php
@@ -1,12 +1,20 @@
 <?php
+/* Where the kawf tree sits in the filesystem */
 $srcroot = "/home/jerdfelt/software";
+
+/* Where all of the templates reside */
 $template_dir = "/web/kawf.org/forums/dev/templates/";
 
-# This is optional. Don't declare the variable if you don't want it added
+/* This is optional. Don't declare the variable if you don't want it added */
 $include_append = "/home/jerdfelt/software/php";
 
-# At audiworld.com, we have different configs for production and development
-# sites, so we can have correct URL's, etc
+/* Style for guests and initial for new users */
+$default_style = "classic";
+
+/*
+ * At audiworld.com, we have different configs for production and development
+ * sites, so we can have correct URL's, etc
+ */
 $config = "config";
 
 include("$srcroot/kawf/user/main.php");
