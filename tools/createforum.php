eb28c288749279537caab069ce0730e5b73d2b5d
jerdfelt <jerdfelt>
2001-04-27 05:23:57 +0000

    Make it worthwhile

--- tools/createforum.php
+++ tools/createforum.php
@@ -1,12 +1,23 @@
 #!/usr/bin/php -q
 <?php
 
+/* First setup the path */
+$include_path = "..:../include:../../php";
+$old_include_path = ini_get("include_path");
+if (!empty($old_include_path))
+  $include_path .= ":" . $old_include_path;
+ini_set("include_path", $include_path);
+
+include("config.inc");
 include("sql.inc");
 include("user/tables.inc");
 
 set_time_limit(0);
 
-sql_open();
+sql_open($database);
+
+$name = "Test forum";
+$shortname = "test";
 
 if (!isset($name) || empty($name)) {
   echo "Please specify a name\n";
@@ -21,7 +32,7 @@ if (!isset($shortname) || empty($shortname)) {
 sql_query("insert into f_forums ( name, shortname ) values ( '" . addslashes($name) . "', '" . addslashes($shortname) . "' )");
 $fid = sql_query1("select last_insert_id()");
 
-sql_query("insert into f_indexes ( fid, minmid, maxmid, mintid, maxtid, active, moderated, deleted ) values ( $fid, 0, 0, 0, 0, 0, 0, 0 )";
+sql_query("insert into f_indexes ( fid, minmid, maxmid, mintid, maxtid, active, moderated, deleted ) values ( $fid, 0, 0, 0, 0, 0, 0, 0 )");
 $iid = sql_query1("select last_insert_id()");
 
 sql_query(sprintf($create_message_table, $iid));
