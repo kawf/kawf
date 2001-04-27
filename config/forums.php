811068d349bc90ceff2b4f56b36e1d81075381a6
jerdfelt <jerdfelt>
2001-04-27 01:00:08 +0000

    Update the examples to be current and not audiworld specific

--- config/forums.php
+++ config/forums.php
@@ -1,8 +1,12 @@
 <?php
 $srcroot = "/home/jerdfelt/software";
-$template_dir = "/web/audiworld.com/forums/dev/templates/";
+$template_dir = "/web/kawf.org/forums/dev/templates/";
 
-# We prod and dev for the different version
+# This is optional. Don't declare the variable if you don't want it added
+$include_append = "/home/jerdfelt/software/php";
+
+# At audiworld.com, we have different configs for production and development
+# sites, so we can have correct URL's, etc
 $config = "config";
 
 include("$srcroot/kawf/user/main.php");
