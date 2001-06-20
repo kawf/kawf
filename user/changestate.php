ec45b62f9236ab403a51b5db94b5e01e6deea7e0
jerdfelt <jerdfelt>
2001-06-20 02:14:51 +0000

    Allow the owner to delete their message again (since we redirect from delete.phtml now) Update the totals when changing state for that user

--- user/changestate.php
+++ user/changestate.php
@@ -1,13 +1,15 @@
 <?php
 
+$user->req();
+
 $index = find_msg_index($mid);
 
-if (!$user->moderator($forum['fid'])) {
+$msg = sql_querya("select aid, pid, state from f_messages$index where mid = '" . addslashes($mid) . "'");
+
+if ($user->aid != $msg['aid'] && !$user->moderator($forum['fid'])) {
   echo "You are not allowed to change the state of this message\n";
   exit;
 }
-
-$msg = sql_querya("select aid, pid, state from f_messages$index where mid = '" . addslashes($mid) . "'");
 
 if (!isset($msg['pmid']))
   $msg['pmid'] = $msg['pid'];
@@ -16,6 +18,10 @@ sql_query("update f_messages$index set " .
 	"changes = CONCAT(changes, 'Changed to $state from ', state, ' by " . $user->name . "/" . $user->aid . " at ', NOW(), '\n'), " .
 	"state = '$state' " .
 	"where mid = '" . addslashes($mid) . "'");
+
+/* Update the posting totals for this user */
+$user->post($forum['fid'], $state, 1);
+$user->post($forum['fid'], $msg['state'], -1);
 
 if ($msg['pmid'] == 0)
   sql_query("update f_indexes set " . $msg['state'] . " = " . $msg['state'] . " - 1, $state = $state + 1 where iid = $index");
