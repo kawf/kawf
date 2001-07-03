8fb14a93eec252a253f4fc8b5d47402cb1a1286d
jerdfelt <jerdfelt>
2001-07-03 21:22:23 +0000

    Use AccountUser, not ForumUser class

--- user/account/logout.php
+++ user/account/logout.php
@@ -1,5 +1,10 @@
 <?php
 
+$aid = $user->aid;
+
+$user = new AccountUser();
+$user->find_by_aid((int)$aid);
+
 $user->unsetcookie();
 
 header("Location: login.phtml?message=" . urlencode("You have been logged out"));
