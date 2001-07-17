a6685a6f474536aa0e66668b42ea78d19b2ec7a3
jerdfelt <jerdfelt>
2001-07-17 18:24:50 +0000

    Examples for configuration

---
+++ examples/forums.php
@@ -0,0 +1,9 @@
+<?php
+$srcroot = "/home/jerdfelt/software";
+$template_dir = "/web/forums.kawf.org/templates/";
+
+# We use prod and dev for the different versions
+$config = "config";
+
+include("$srcroot/kawf/user/main.php");
+?>
---
+++ examples/nightly.php
@@ -0,0 +1,8 @@
+<?php
+$srcroot = "/home/jerdfelt/software";
+
+# We use prod and dev for the different versions
+$config = "config";
+
+include("$srcroot/kawf/nightly.php");
+?>
