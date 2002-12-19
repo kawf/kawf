9768371203ebceb5df8923432207e51405e2eff5
jerdfelt <jerdfelt>
2002-12-19 01:38:43 +0000

    Nuke this directory

--- examples/forums.php
+++
@@ -1,9 +0,0 @@
-<?php
-$srcroot = "/home/jerdfelt/software";
-$template_dir = "/web/forums.kawf.org/templates/";
-
-# We use prod and dev for the different versions
-$config = "config";
-
-include("$srcroot/kawf/user/main.php");
-?>
--- examples/nightly.php
+++
@@ -1,8 +0,0 @@
-<?php
-$srcroot = "/home/jerdfelt/software";
-
-# We use prod and dev for the different versions
-$config = "config";
-
-include("$srcroot/kawf/nightly.php");
-?>
