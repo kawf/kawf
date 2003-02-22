ecb5a80bd7b5b4437f55ebe419a55949e5840700
jerdfelt <jerdfelt>
2003-02-22 00:36:29 +0000

    Fixup multiple entries support Don't let user change fid, since it's used as a key

--- admin/useraclmodify.php
+++ admin/useraclmodify.php
@@ -5,22 +5,24 @@ $user->req("ForumAdmin");
 /* If submit is set, shove the data into the database (well, after some */
 /* error checking) */
 if (isset($submit)) {
-  $capabilities = Array();
+  for ($i = 0; $i < $count; $i++) {
+    $capabilities = Array();
 
-  if (isset($Lock))
-    $capabilities[] = "Lock";
-  if (isset($Moderate))
-    $capabilities[] = "Moderate";
-  if (isset($Delete))
-    $capabilities[] = "Delete";
-  if (isset($OffTopic))
-    $capabilities[] = "OffTopic";
-  if (isset($Advertise))
-    $capabilities[] = "Advertise";
+    if (isset($opt[$i]['Lock']))
+      $capabilities[] = "Lock";
+    if (isset($opt[$i]['Moderate']))
+      $capabilities[] = "Moderate";
+    if (isset($opt[$i]['Delete']))
+      $capabilities[] = "Delete";
+    if (isset($opt[$i]['OffTopic']))
+      $capabilities[] = "OffTopic";
+    if (isset($opt[$i]['Advertise']))
+      $capabilities[] = "Advertise";
 
-  $capabilities = join(",", $capabilities);
+    $capabilities = join(",", $capabilities);
 
-  sql_query("update f_moderators set fid = " . addslashes($fid) . ", capabilities = '" . addslashes($capabilities) . "'");
+    sql_query("update f_moderators set capabilities = '" . addslashes($capabilities) . "' where aid = " . addslashes($aid) . " and fid = " . addslashes($fid));
+  }
 
   Header("Location: useracl.phtml?message=" . urlencode("User ACL Modified"));
   exit;
@@ -52,18 +54,29 @@ while ($acl = sql_fetch_array($result)) {
     $capabilities[$value] = true;
 ?>
 
+<input type="hidden" name="opt[<?php echo $count; ?>][fid]" value="<?php echo $acl['fid']; ?>">
+
  <tr>
-  <td>fid</td>
-  <td><input type="text" name="opts[$count][fid]" value="<?php echo $acl['fid']; ?>"></td>
+<?php
+  if ($acl['fid'] == -1) {
+?>
+  <td>All Forums</td>
+<?php
+  } else {
+?>
+  <td>Forum <?php echo $acl['fid']; ?></td>
+<?php
+  }
+?>
  </tr>
  <tr>
   <td>Capabilities</td>
   <td>
-    <input type="checkbox" name="opts[$count][Lock]"<?php if (isset($capabilities['Lock'])) echo " checked"; ?>> Lock Threads<br>
-    <input type="checkbox" name="opts[$count][Moderate]"<?php if (isset($capabilities['Moderate'])) echo " checked"; ?>> Moderate Messages<br>
-    <input type="checkbox" name="opts[$count][Delete]"<?php if (isset($capabilities['Delete'])) echo " checked"; ?>> Delete Messages<br>
-    <input type="checkbox" name="opts[$count][OffTopic]"<?php if (isset($capabilities['OffTopic'])) echo " checked"; ?>> Mark Threads Off-Topic<br>
-    <input type="checkbox" name="opts[$count][Advertise]"<?php if (isset($capabilities['Advertise'])) echo " checked"; ?>> Can Advertise<br>
+    <input type="checkbox" name="opts[<?php echo $count; ?>][Lock]"<?php if (isset($capabilities['Lock'])) echo " checked"; ?>> Lock Threads<br>
+    <input type="checkbox" name="opts[<?php echo $count; ?>][Moderate]"<?php if (isset($capabilities['Moderate'])) echo " checked"; ?>> Moderate Messages<br>
+    <input type="checkbox" name="opts[<?php echo $count; ?>][Delete]"<?php if (isset($capabilities['Delete'])) echo " checked"; ?>> Delete Messages<br>
+    <input type="checkbox" name="opts[<?php echo $count; ?>][OffTopic]"<?php if (isset($capabilities['OffTopic'])) echo " checked"; ?>> Mark Threads Off-Topic<br>
+    <input type="checkbox" name="opts[<?php echo $count; ?>][Advertise]"<?php if (isset($capabilities['Advertise'])) echo " checked"; ?>> Can Advertise<br>
   </td>
  </tr>
 
@@ -77,6 +90,7 @@ while ($acl = sql_fetch_array($result)) {
   <td><input type="submit" name="submit" value="Update"></td>
  </tr>
 </table>
+<input type="hidden" name="count" value="<?php echo $count; ?>">
 </form>
 
 <?php
