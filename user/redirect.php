3de63f34d97b6c846253d6521c1959afdd653c60
Gary Gabriel <ggabriel@gmail.com>
2009-03-23 17:57:54 -0400

    Enable multiple forum redirects.
    Modify redirect.php to accept an array of URLs and redirect to the first
    non-empty URL.  Add a sample template utilizing this redirect to allow
    non-javascript use of the forum menus.

--- user/redirect.php
+++ user/redirect.php
@@ -1,3 +1,17 @@
 <?php
-header("Location: $url");
+if(is_array($url)) {
+  // Redirect to the first non-empty value in the array.
+  $oneurl = "";
+  foreach($url as $oneurl) {
+    if($oneurl) {
+      break;
+    }
+  }
+  if(!$oneurl) {
+    $oneurl = "/tracking.phtml";
+  }
+  header("Location: $oneurl");
+} else {
+  header("Location: $url");
+}
 ?>
