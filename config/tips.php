b5cdb1543155fedc36f0799bc590fe73fe28fd88
kawf Role Account <kawf@u15332770.onlinehome-server.com>
2009-03-23 22:30:16 -0400

    add tip about auto track thread in prefs

--- config/tips.php
+++ config/tips.php
@@ -6,17 +6,16 @@
 <body>
 
 <h1>Posting Tips</h1>
-<br>
-<p>There are two ways to add a picture to a Forum posting.  The first is to use the box in the posting dialogue called "Optional Image URL".  If you put the URL to a picture in this box then it will appear within your post.  
+<p>To automatically track threads that you create or post to, make sure to check the "Default to track threads you create or followup to" checkbox in your <a href="/preferences.phtml?page=/tips/">preferences</a>.</p>
 
-<p>The second way is to use the appropriate HTML within the body of the message itself.  This can be useful for posting multiple pictures in the same post.  Here is the sample code:
+<p>There are two ways to add a picture to a Forum posting.  The first is to use the box in the posting dialogue called "Optional Image URL".  If you put the URL to a picture in this box then it will appear within your post. The second way is to use the appropriate HTML within the body of the message itself.  This can be useful for posting multiple pictures in the same post.  Here is the sample code:
 
 <p>&lt;img src="http://www.the_location_of_your_pic.com/your_pic_name1.jpg"&gt;
 <br>&lt;img src="http://www.the_location_of_your_pic.com/your_pic_name2.jpg"&gt;
 <br>&lt;img src="http://www.the_location_of_your_pic.com/your_pic_name3.jpg"&gt;
 <br>
 <br>
-be nice when you code your own hyper links;
+Be nice when you code your own hyper links;
 <br>
 &lt;a href="url here" target="_blank"&gt;&lt;img src="&lt;url here&gt;"&gt;&lt;/a&gt;
 <br>
