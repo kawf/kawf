2d1b25242c2687b0b36778dba5985033370c9063
Nye Liu <nyet@curtisfong.org>
2009-04-16 12:46:07 -0700

    postprocess .txt the same way we would for .phtml

--- user/plainmessage.php
+++ user/plainmessage.php
@@ -1,13 +1,15 @@
 <?php
 
+require_once("message.inc");
+
 /* Grab the actual message */
 $index = find_msg_index($mid);
-$sql = "select message from f_messages" . $indexes[$index]['iid'] . " where mid = '" . addslashes($mid) . "'";
+$sql = "select message,url,urltext from f_messages" . $indexes[$index]['iid'] . " where mid = '" . addslashes($mid) . "'";
 $result = mysql_query($sql) or sql_error($sql);
 
 $msg = mysql_fetch_array($result);
 
 header("Content-type: text/plain");
-echo $msg['message'];
+echo postprocess($msg);
 
 ?>
