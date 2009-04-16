3aa645c79cc72537e4bdc5539b793164f27422b4
Nye Liu <nyet@curtisfong.org>
2009-04-16 09:24:15 -0700

    move path set up back in here.
    eventually, centralize it somewhere useful.

--- nightly.php
+++ nightly.php
@@ -5,6 +5,19 @@ if (!isset($srcroot)) {
     exit(1);
 }
 
+/* First setup the path */
+$include_path = "$srcroot:$srcroot/lib:$srcroot/include:$srcroot/user";
+if (!isset($dont_use_account))
+  $include_path .= ":" . "$srcroot/user/account";
+
+if (isset($include_append))
+  $include_path .= ":" . $include_append;
+
+$old_include_path = ini_get("include_path");
+if (!empty($old_include_path))
+  $include_path .= ":" . $old_include_path;
+ini_set("include_path", $include_path);
+
 include_once("$config.inc");
 require_once("sql.inc");
 require_once("util.inc");
