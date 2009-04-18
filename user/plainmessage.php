c40d7f77104a0268beceb323cd590dbd570819b5
Nye Liu <nyet@curtisfong.org>
2009-04-17 17:26:08 -0700

    fix output typo. add "raw" option to allow the plain message, no bells and whistles, no db update, no url/urltext, etc.

--- user/plainmessage.php
+++ user/plainmessage.php
@@ -2,16 +2,22 @@
 require_once("template.inc");
 require_once("message.inc");
 
+$raw = isset($_REQUEST['raw']);
+
 $msg = fetch_message($user, $mid, 'message,url,urltext,tid');
+
+if ($raw) {
+    header("Content-type: text/plain");
+    echo $msg['message'];
+    exit;
+}
 
 mark_thread_read($msg, $user);
 
-header("Content-type: text/plain");
-
-$msg=postprocess($msg);
+$m=postprocess($msg);
 
 $tpl = new Template($template_dir, "comment");
 $tpl->set_file("plain", "plain-message.tpl");
-$tpl->set_var("MSG_MESSAGE", postprocess($msg));
+$tpl->set_var("MSG_MESSAGE", $m);
 $tpl->pparse("MSG_MESSAGE", "plain");
 ?>
