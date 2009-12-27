2d18935bd4f1076ee2401f1cc1026ed1c6993f5c
Gary Gabriel <ggabriel@gmail.com>
2009-12-27 00:57:07 -0500

    Set schema version during initialization.

--- tools/initial.php
+++ tools/initial.php
@@ -33,7 +33,9 @@ sql_query($create_offtopic_table);
 sql_query($create_preferences_table);
 sql_query($create_user_preferences_table);
 sql_query($create_global_messages_table);
+
 sql_query($create_schema_version_table);
+sql_query($set_current_schema_version);
 
 /* Static preferences. */
 sql_query($insert_static_preferences);
