75a3447d09c8589558321dd8dd1f8720ca89b1ac
Gary Gabriel <ggabriel@gmail.com>
2010-01-01 23:02:02 -0500

    Make ids larger and unsigned for ACL ip tables.

---
+++ db/migrations/20100101211822_make_acl_ids_bigint.php
@@ -0,0 +1,15 @@
+<?
+
+class MakeAclIdsBigint extends DatabaseMigration {
+  public function migrate() {
+    $sql = "ALTER TABLE acl_ips " .
+           "MODIFY COLUMN id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT";
+    $this->execute_sql($sql);
+
+    $sql = "ALTER TABLE acl_ip_bans " .
+           "MODIFY COLUMN id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT";
+    $this->execute_sql($sql);
+  }
+}
+
+?>
