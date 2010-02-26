5a8bbf036567bf8261310c23b0d526530d486418
Nye Liu <nyet@curtisfong.org>
2010-02-25 22:23:00 -0800

    header(Location:) doesn't hide referer. Use meta refresh

--- user/redirect.php
+++ user/redirect.php
@@ -13,8 +13,23 @@ if(is_array($url)) {
   if(!$oneurl) {
     $oneurl = "/tracking.phtml";
   }
-  header("Location: $oneurl");
+  $url = $oneurl;
+}
+
+if (isset($_REQUEST['refresh']) ) {
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta http-equiv="refresh" content="0; URL=<? echo $url; ?>">
+	<title></title>
+</head>
+<body><p><a href="<? echo $url; ?>">Link</a></p></body>
+</html>
+<?
 } else {
-  header("Location: $url");
+    header("Location: $url");
 }
 ?>
