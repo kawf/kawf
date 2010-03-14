70c31daf7610997f0fca9c3bb7f2a8cb5fa988b8
Nye Liu <nyet@curtisfong.org>
2010-03-13 21:24:57 -0800

    missed one

--- user/post.php
+++ user/post.php
@@ -75,7 +75,7 @@ if (!$user->capable($forum['fid'], 'Delete')) {
       exit;
     }
   } else {
-    if (!isset($forum['opt.PostReply'])) {
+    if (!isset($forum['option']['PostReply'])) {
       $tpl->set_var(array(
         "locked" => "",
         "error" => "",
