5b7ca68ed535546796fd89298d41161f989828be
Nye Liu <nyet@stupid.curtisfong.org>
2010-03-15 15:35:17 -0700

    traverse all shards of a forum, not just the first

--- db/migrations/20100222212442_add_video_url.php
+++ db/migrations/20100222212442_add_video_url.php
@@ -2,10 +2,10 @@
 
 class AddVideoUrl extends DatabaseMigration {
   public function migrate() {
-    $ret = sql_query("select fid from f_forums order by f_forums.fid");
+    $ret = sql_query("select iid from f_indexes order by iid");
     echo "DO NOT INTERRUPT, this could take quite some time!\n";
-    while ($f = sql_fetch_array($ret) ) {
-	$tbl = "f_messages" . $f['fid'];
+    while ($i = sql_fetch_array($ret) ) {
+	$tbl = "f_messages" . $i['iid'];
 	$sql = "ALTER TABLE $tbl " .
 	       "ADD COLUMN video VARCHAR(250) NOT NULL DEFAULT ''" .
 	       "AFTER urltext, " .
