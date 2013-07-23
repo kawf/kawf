8c694172e7d40d8f3d17032be809f9f16c46fd86
Nye Liu <nyet@nyet.org>
2013-07-23 13:32:21 -0700

    Add -n dry run flag, add -l and -p flag to only affect messages with links or pictures

--- tools/delete_by_user.php
+++ tools/delete_by_user.php
@@ -7,10 +7,26 @@ include_once($kawf_base . "/include/sql.inc");
 
 // date_default_timezone_set("America/Los_Angeles");
 
-$opts = getopt('u:');
+$opts = getopt('u:nlp');
 if(!array_key_exists('u', $opts) or !($aid = (int)$opts['u'])) {
   echo "you must supply -u <aid>\n";
   exit(1);
+}
+
+$dry_run=array_key_exists('n', $opts);
+$where = "aid = $aid AND state <> 'Deleted'";
+$where_flags = array();
+
+if (array_key_exists('l', $opts)) {
+    $where_flags[] = "FIND_IN_SET('Link', flags)";
+}
+
+if (array_key_exists('p', $opts)) {
+    $where_flags[] = "FIND_IN_SET('Picture', flags)";
+}
+
+if (count($where_flags)) {
+    $where .= " AND (" . join(' OR ', $where_flags) . ")";
 }
 
 sql_open($database);
@@ -33,16 +49,37 @@ foreach($tables as $fid) {
   $changes = 'Changed to Deleted from Active by ' . get_current_user() . ' using delete_by_user.php at ' .
     date('Y-m-d H:i:s');
 
-  sql_execute("replace into f_upostcount (aid, fid, status, count ) values ( '$aid', '$fid', 'Deleted', '$count' )");
-  sql_execute("replace into f_upostcount (aid, fid, status, count ) values ( '$aid', '$fid', 'Active', '0' )");
+  if ($dry_run) {
+      $cmd = "select count(*) from $table WHERE $where";
+      printf("'$cmd'\n");
+      $count = sql_query1($cmd);
+      printf("Matched %d messages\n", $count);
+  } else {
+      sql_execute_wrapper(
+	"UPDATE $table SET state = 'Deleted', " .
+	"flags = CONCAT_WS(',', IF(flags = '', NULL, flags), 'StateLocked'), " .
+	"changes = CONCAT_WS('\\n', changes, '$changes') " .
+	"WHERE $where"
+      );
+      printf("Deleted %d messages\n", sql_affected_rows());
+  }
 
-  sql_execute(
-    "UPDATE $table SET state = 'Deleted', " .
-    "flags = CONCAT_WS(',', IF(flags = '', NULL, flags), 'StateLocked'), " .
-    "changes = CONCAT_WS('\\n', changes, '$changes') " .
-    "WHERE aid = $aid AND state <> 'Deleted'"
-  );
+  $deleted = sql_query1("select count(*) from $table where aid = $aid AND state = 'Deleted'");
+  $active = sql_query1("select count(*) from $table where aid = $aid AND state = 'Active'");
 
-  printf("Deleted %d messages\n", sql_affected_rows());
+  sql_execute_wrapper("replace into f_upostcount (aid, fid, status, count ) values ( '$aid', '$fid', 'Deleted', '$deleted' )");
+  sql_execute_wrapper("replace into f_upostcount (aid, fid, status, count ) values ( '$aid', '$fid', 'Active', '$active' )");
 }
+
+function sql_execute_wrapper($cmd)
+{
+    global $dry_run;
+
+    if ($dry_run) {
+	printf("dry run '$cmd'\n");
+    } else {
+	sql_execute($cmd);
+    }
+}
+
 ?>
