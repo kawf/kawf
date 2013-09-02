de59549c1d9d10ae6ff120990a24a62e4a157003
Gary Gabriel <gg@bitmelt.com>
2013-09-02 16:56:56 -0400

    Use PDO in tools/optimize.php

--- tools/optimize.php
+++ tools/optimize.php
@@ -1,35 +1,20 @@
 #!/usr/bin/php -q
 <?php
+$kawf_base = realpath(dirname(__FILE__) . "/..");
+require_once($kawf_base . "/config/config.inc");
+require_once($kawf_base . "/include/sql.inc");
 
-/* First setup the path */
-/* $include_path = "..:../include:../../php"; */
-$include_path = "..:../include:../config";
-$old_include_path = ini_get("include_path");
-if (!empty($old_include_path))
-  $include_path .= ":" . $old_include_path;
-ini_set("include_path", $include_path);
-
-require_once("config.inc");
-require_once("sql.inc");
-
-sql_open($database);
+db_connect();
 
 if(!ini_get('safe_mode'))
     set_time_limit(0);
 
-$alltables = sql_query("SHOW TABLES");
-if(!$alltables) die("show tables fail: " . sql_error() . "\n");
-
-while ($table = mysql_fetch_assoc($alltables))
-{
-   foreach ($table as $db => $tablename)
-   {
-      echo "optimize table $tablename\n";
-      sql_query("OPTIMIZE TABLE $tablename")
-          or die(mysql_error());
-   }
+$sth = db_query("SHOW TABLES");
+while ($row = $sth->fetch()) {
+  $tablename = $row[0];
+  echo "optimize table $tablename\n";
+  db_exec("OPTIMIZE TABLE $tablename");
 }
-
-sql_close();
+$sth->closeCursor();
 
 ?>
