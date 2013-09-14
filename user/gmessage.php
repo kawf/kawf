b6f9961813d7a6989f619e8ed1251016321354ae
Gary Gabriel <gg@bitmelt.com>
2013-09-14 12:39:48 -0400

    Use PDO in user/gmessage.php

--- user/gmessage.php
+++ user/gmessage.php
@@ -10,7 +10,7 @@ if (!isset($gid) || is_int($gid) || $gid < -1 || $gid > 63) {
     err_not_found();
 }
 
-$gmsg = sql_querya("select * from f_global_messages where gid = '" . addslashes($gid) . "'");
+$gmsg = db_query_first("select * from f_global_messages where gid = ?", array($gid));
 
 if (strlen($_REQUEST['state'])>0) {
     if (!$gmsg || !$user->admin()) {
@@ -22,9 +22,9 @@ if (strlen($_REQUEST['state'])>0) {
     }
 
     if ($_REQUEST['state'] == "Active") {
-	sql_query("update f_global_messages set state = 'Active' where gid = '" . addslashes($gid) . "'");
+	db_exec("update f_global_messages set state = 'Active' where gid = ?", array($gid));
     } elseif ($_REQUEST['state'] == "Inactive") {
-	sql_query("update f_global_messages set state = 'Inactive' where gid = '" . addslashes($gid) . "'");
+	db_exec("update f_global_messages set state = 'Inactive' where gid = ?", array($gid));
     } else {
 	err_not_found();
     }
@@ -37,17 +37,17 @@ if (isset($_REQUEST['hide'])) {
 
     if ($gid==-1) {
 	if ($_REQUEST['hide']==1) {	/* hide all */
-	      sql_query("update u_users set gmsgfilter = " . (~0) ." where aid = '" . $user->aid . "'");
+	      db_exec("update u_users set gmsgfilter = " . (~0) ." where aid = ?", array($user->aid));
 	} else {			/* show all */
-	      sql_query("update u_users set gmsgfilter = 0 where aid = '" . $user->aid . "'");
+	      db_exec("update u_users set gmsgfilter = 0 where aid = ?", array($user->aid));
 	}
     } elseif ($gmsg) {
 	if ($_REQUEST['hide']==1) {	/* hide gid */
 	    if (!($user->gmsgfilter & (1 << $gid)))
-	      sql_query("update u_users set gmsgfilter = " . ($user->gmsgfilter | (1 << $gid)) . " where aid = '" . $user->aid . "'");
+	      db_exec("update u_users set gmsgfilter = " . ($user->gmsgfilter | (1 << $gid)) . " where aid = ?", array($user->aid));
 	} elseif ($_REQUEST['hide']==0) { /* show gid */
 	    if ($user->gmsgfilter & (1 << $gid))
-	      sql_query("update u_users set gmsgfilter = " . ($user->gmsgfilter & ~(1 << $gid)) . " where aid = '" . $user->aid . "'");
+	      db_exec("update u_users set gmsgfilter = " . ($user->gmsgfilter & ~(1 << $gid)) . " where aid = ?", array($user->aid));
 	}
     } else {
 	err_not_found();
