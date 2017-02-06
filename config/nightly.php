cfca2e062e128f22e3cc35c9e4a587dea111881a
Nye Liu <nyet@nyet.org>
2017-02-05 22:52:53 -0800

    Return error so if the wrong nightly.php is being run from cron the user is notified.
    TODO: remove entirely.

--- config/nightly.php
+++ config/nightly.php
@@ -3,4 +3,5 @@ require("setup.inc");
 /* Don't actually run this... we don't want a misconfigured apache server running it */
 echo "Deprecated. Instead:\n";
 echo "php $srcroot/tools/nightly.php\n";
+exit(1)
 ?>
