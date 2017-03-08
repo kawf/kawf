ec070840976720ff13fc1da10fe49b465fdf3449
Nye Liu <nyet@nyet.org>
2017-03-08 14:32:10 -0800

    Use full content-type/charset specifier

--- config/tips.php
+++ config/tips.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-<meta charset="utf-8"/>
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
 <title>Forum Tips</title>
 <link rel=StyleSheet href="<?php echo css_href() ?>" type="text/css">
 </head>
