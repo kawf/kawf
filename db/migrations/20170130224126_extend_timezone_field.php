34a2073daaf687d58715c85010a936f8e36b2d15
Nye Liu <nyet@nyet.org>
2017-10-10 12:28:02 -0700

    Remove charset and collation from migrations

--- db/migrations/20170130224126_extend_timezone_field.php
+++ db/migrations/20170130224126_extend_timezone_field.php
@@ -2,7 +2,7 @@
 
 class ExtendTimezoneField extends DatabaseMigration {
   public function migrate() {
-    $sql = "ALTER TABLE `u_users` CHANGE `timezone` `timezone` VARCHAR(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT ''";
+    $sql = "ALTER TABLE `u_users` CHANGE `timezone` `timezone` VARCHAR(20) NOT NULL DEFAULT ''";
     db_exec($sql);
   }
 }
--- db/migrations/20171010114334_add_u_users_default_pref_value.php
+++ db/migrations/20171010114334_add_u_users_default_pref_value.php
@@ -2,7 +2,7 @@
 
 class AddUUsersDefaultPrefValue extends DatabaseMigration {
   public function migrate() {
-    $sql = "ALTER TABLE `u_users` CHANGE `preferences` `preferences` SET('ShowOffTopic','ShowModerated','Collapsed','SecretEmail','FlatThread','SimpleHTML','AutoTrack','HideSignatures','AutoUpdateTracking','OldestFirst','SortbyActive','CollapseOffTopic','RelativeTimestamps') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT ''";
+    $sql = "ALTER TABLE `u_users` CHANGE `preferences` `preferences` SET('ShowOffTopic','ShowModerated','Collapsed','SecretEmail','FlatThread','SimpleHTML','AutoTrack','HideSignatures','AutoUpdateTracking','OldestFirst','SortbyActive','CollapseOffTopic','RelativeTimestamps') NOT NULL DEFAULT ''";
     db_exec($sql);
   }
 }
