1aee6cedd866d1b27b337bdd805a52bb4709451a
Nye Liu <nyet@nyet.org>
2018-02-16 17:56:59 -0800

    JQuery slim build is missing ajax

--- user/page-yatt.inc.php
+++ user/page-yatt.inc.php
@@ -10,7 +10,7 @@ function generate_page($title, $contents, $skip_header=false, $meta_robots=false
     $page->set('css_href', css_href());
     $page->set('skin_css_href', skin_css_href());
     $page->set('js_href', js_href());
-    $page->set('js_jquery_href', js_href($filename="jquery-3.3.1.slim.min.js", $cache_buster=false));
+    $page->set('js_jquery_href', js_href($filename="jquery-3.3.1.min.js", $cache_buster=false));
     $bch = browser_css_href();
     if ($bch) {
 	$page->set('browser_css_href', $bch);
