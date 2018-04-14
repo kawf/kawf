3ebea29c20e073387778902d3e8a02296a22d323
rfackett <ryan@foghornconsulting.com>
2018-04-14 16:18:13 -0700

    build includes security updates now

--- chef/metadata.rb
+++ chef/metadata.rb
@@ -2,7 +2,7 @@ name             'kawf'
 maintainer       'squirrels'
 license          'Apache 2.0'
 description      'kawf forum'
-version          '0.17.0'
+version          '0.23.0'
 
 depends 'apt'
 depends 'git'
