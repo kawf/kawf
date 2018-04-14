fd6eb1e5b2031c8b057eafc536a928d48913f5e3
Nye Liu <nyet@nyet.org>
2018-04-14 16:16:16 -0700

    Add default timezone value to u_users table

---
+++ db/migrations/20180414161247_add_u_users_default_value.php
@@ -0,0 +1,10 @@
+<?php
+
+class AddUUsersDefaultValue extends DatabaseMigration {
+  public function migrate() {
+    $sql = "ALTER TABLE `u_users` CHANGE `timezone` `timezone` VARCHAR(20) NOT NULL DEFAULT ''";
+    db_exec($sql);
+  }
+}
+
+?>
