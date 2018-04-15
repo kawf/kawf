c4f1703fc06bcb57e46ee2e8adc5bb583ffce8c2
Nye Liu <nyet@nyet.org>
2018-04-15 01:41:24 -0700

    Clean up bad timestamps in f_dupposts

--- db/migrations/20180415011839_add_ipv6_support.php
+++ db/migrations/20180415011839_add_ipv6_support.php
@@ -5,6 +5,12 @@ class AddIpV6Support extends DatabaseMigration {
     $sql = "ALTER TABLE f_visits " .
 	   "CHANGE `ip` `ip` varchar(47) NOT NULL";
     db_exec($sql);
+    $sql = "UPDATE f_dupposts " .
+	   "SET `tstamp` = '1970-01-01 00:00:00' WHERE `tstamp` = CONVERT(0,DATETIME)";
+    db_exec($sql);
+    $sql = "ALTER TABLE f_dupposts " .
+	   "CHANGE `tstamp` `tstamp` datetime not null default CURRENT_TIMESTAMP";
+    db_exec($sql);
     $sql = "ALTER TABLE f_dupposts " .
 	   "CHANGE `ip` `ip` varchar(47) NOT NULL";
     db_exec($sql);
