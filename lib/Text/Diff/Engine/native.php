0db7f1e16544da1b4d417385ae57c13b057d8267
Nye Liu <nyet@nyet.org>
2019-01-30 09:22:57 -0800

    Replace deprecated each()'s in strip and diff engine

--- lib/Text/Diff/Engine/native.php
+++ lib/Text/Diff/Engine/native.php
@@ -192,7 +192,8 @@ class Text_Diff_Engine_native {
                 }
                 $matches = $ymatches[$line];
                 reset($matches);
-                while (list(, $y) = each($matches)) {
+                //while (list(, $y) = each($matches)) {
+                while ($y = next($matches)) {
                     if (empty($this->in_seq[$y])) {
                         $k = $this->_lcsPos($y);
                         assert($k > 0);
@@ -200,7 +201,8 @@ class Text_Diff_Engine_native {
                         break;
                     }
                 }
-                while (list(, $y) = each($matches)) {
+                //while (list(, $y) = each($matches)) {
+                while ($y = next($matches)) {
                     if ($y > $this->seq[$k - 1]) {
                         assert($y <= $this->seq[$k]);
                         /* Optimization: this is a common case: next match is
